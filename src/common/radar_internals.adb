7efc9a2d1a0d48fb328d27d4ec3654ea4bb9130c
Léo Germond <germond@adacore.com>
2023-05-10 22:23:30 +0200

    internal: fix

--- src/common/radar_internals.adb
+++ src/common/radar_internals.adb
@@ -9,21 +9,22 @@ package body Radar_Internals is
    Antenna_Angle : Angle_Degrees_T := 120;
 
    type Object_Type_T is (Sarah_Connor, Peon, John_Connor);
-   Active_Object_Type     : Object_Type_T        := Sarah_Connor;
-   Active_Object_Status   : Object_Status_T      := Out_Of_Range;
+   Active_Object_Type : Object_Type_T := Sarah_Connor;
+   Active_Object_Status : Object_Status_T := Out_Of_Range;
    Active_Object_Distance : Object_Distance_Km_T := 9.0;
 
    Current_Walk_Speed : Speed_Setting_T;
 
-   Has_E_T_A       : Boolean := False;
-   Time_To_Arrival : Float   := 0.0;
+   Has_E_T_A : Boolean := False;
+   Time_To_Arrival : Float := 0.0;
 
    procedure Rotate_Antenna (Speed : Speed_Setting_T) is
    begin
-      Antenna_Angle :=
-        Antenna_Angle +
-        (case Speed is when Slow => 7, when Normal => 17, when Fast => 59,
-           when Stopped => 0);
+      Antenna_Angle := Antenna_Angle + (case Speed is
+                                           when Slow => 7,
+                                           when Normal => 17,
+                                           when Fast => 59,
+                                           when Stopped => 0);
    end Rotate_Antenna;
 
    procedure Walk_And_Scan is
@@ -43,13 +44,13 @@ package body Radar_Internals is
    procedure Next_Object is
    begin
       Active_Object_Distance := 9.0;
-      Active_Object_Status   := Out_Of_Range;
+      Active_Object_Status := Out_Of_Range;
 
       if Active_Object_Type = Object_Type_T'Last then
          Active_Object_Type := Object_Type_T'First;
       else
-         Active_Object_Type :=
-           Object_Type_T'Val (Object_Type_T'Pos (Active_Object_Type) + 1);
+         Active_Object_Type
+           := Object_Type_T'Val (Object_Type_T'Pos (Active_Object_Type) + 1);
       end if;
    end Next_Object;
 
@@ -70,14 +71,16 @@ package body Radar_Internals is
 
    function Get_Running_Speed return Speed_Kph_T is
    begin
-      return
-        (case Current_Walk_Speed is when Stopped => 0.0, when Slow => 4.0,
-           when Normal => 8.0, when Fast => 30.0);
+      return (case Current_Walk_Speed is
+                 when Stopped => 0.0,
+                 when Slow => 4.0,
+                 when Normal => 8.0,
+                 when Fast => 30.0);
    end Get_Running_Speed;
 
    procedure Update_E_T_A (E_T_A : Float) is
    begin
-      Has_E_T_A       := True;
+      Has_E_T_A := True;
       Time_To_Arrival := E_T_A;
    end Update_E_T_A;
 
@@ -88,29 +91,28 @@ package body Radar_Internals is
 
    procedure Time_Step is
    begin
-      Put
-        ("T =" & Natural'Image (T) & "  Antenna" &
-         Angle_Degrees_T'Image (Antenna_Angle) & "deg." & ASCII.HT & "ETA");
+      Put ("T =" & Natural'Image (T)
+           & "  Antenna" & Angle_Degrees_T'Image (Antenna_Angle)
+           & "deg." & ASCII.HT & "ETA");
       if Has_E_T_A then
          declare
-            E_T_A_H : Integer :=
-              Integer (Float'Floor (Time_To_Arrival / 3_600.0));
-            E_T_A_M : Integer :=
-              Integer (Float'Floor (Time_To_Arrival / 60.0)) mod 60;
-            E_T_A_S : Integer :=
-              Integer (Float'Rounding (Time_To_Arrival)) mod 60;
+            E_T_A_H : constant Integer
+              := Integer (Float'Floor (Time_To_Arrival / 3600.0));
+            E_T_A_M : constant Integer
+              := Integer (Float'Floor (Time_To_Arrival / 60.0)) mod 60;
+            E_T_A_S : constant Integer
+              := Integer (Float'Rounding (Time_To_Arrival)) mod 60;
          begin
-            Put
-              (Integer'Image (E_T_A_H) & "h" & Integer'Image (E_T_A_M) & "m" &
-               Integer'Image (E_T_A_S) & "s");
+            Put (Integer'Image (E_T_A_H) & "h"
+                 & Integer'Image (E_T_A_M) & "m"
+                 & Integer'Image (E_T_A_S) & "s");
          end;
 
          if Time_To_Arrival <= 500.0 then
             Active_Object_Distance := Object_Distance_Km_T'First;
          else
-            Active_Object_Distance :=
-              Active_Object_Distance -
-              Object_Distance_Km_T'Min
+            Active_Object_Distance := Active_Object_Distance
+              - Object_Distance_Km_T'Min
                 (Active_Object_Distance - Object_Distance_Km_T'First,
                  Active_Object_Distance * 500.0 / Time_To_Arrival);
          end if;
@@ -119,30 +121,36 @@ package body Radar_Internals is
          Put (" none ");
       end if;
 
-      if Active_Object_Distance < 5.0 and Active_Object_Status = Out_Of_Range
+      if Active_Object_Distance < 5.0
+        and then Active_Object_Status = Out_Of_Range
       then
          Active_Object_Status := Tracked;
       end if;
 
-      Put_Line
-        (ASCII.HT & Object_Type_T'Image (Active_Object_Type) & " is " &
-         Object_Status_T'Image (Active_Object_Status));
+      Put_Line (ASCII.HT & Object_Type_T'Image (Active_Object_Type)
+                & " is " & Object_Status_T'Image (Active_Object_Status));
       T := T + 1;
    end Time_Step;
 
-   procedure Time_Step
-     (Radar_Angle        : Float; Time_To_Arrival : Float;
-      John_Connor_Status : String)
+   procedure Time_Step (Radar_Angle        : Float; Time_To_Arrival : Float;
+                        John_Connor_Status : String)
    is
-      E_T_A_H : Integer := Integer (Float'Floor (Time_To_Arrival / 3_600.0));
-      E_T_A_M : Integer :=
-        Integer (Float'Floor (Time_To_Arrival / 60.0)) mod 60;
-      E_T_A_S : Integer := Integer (Float'Rounding (Time_To_Arrival)) mod 60;
+      pragma Unreferenced (Radar_Angle);
+      E_T_A_H : constant Integer
+        := Integer (Float'Floor (Time_To_Arrival / 3600.0));
+      E_T_A_M : constant Integer
+        := Integer (Float'Floor (Time_To_Arrival / 60.0)) mod 60;
+      E_T_A_S : constant Integer
+        := Integer (Float'Rounding (Time_To_Arrival)) mod 60;
    begin
-      Put_Line
-        ("T =" & Natural'Image (T) & "  " & "ETA" & Integer'Image (E_T_A_H) &
-         "h" & Integer'Image (E_T_A_M) & "m" & Integer'Image (E_T_A_S) & "s" &
-         "  " & "John Connor is " & John_Connor_Status);
+      Put_Line ("T =" & Natural'Image (T)
+                & "  "
+                & "ETA"
+                & Integer'Image (E_T_A_H) & "h"
+                & Integer'Image (E_T_A_M) & "m"
+                & Integer'Image (E_T_A_S) & "s"
+                & "  "
+                & "John Connor is " & John_Connor_Status);
       T := T + 1;
    end Time_Step;
 
--- src/common/radar_internals.ads
+++ src/common/radar_internals.ads
@@ -1,30 +1,30 @@
 package Radar_Internals is
 
-   -- Types
+   --  Types
    type Object_Status_T is (Out_Of_Range, Tracked, Cleared, Selected);
    subtype Object_Distance_Km_T is Float range 0.01 .. 100.0;
    subtype Speed_Kph_T is Float range 0.0 .. 50.0;
    type Speed_Setting_T is (Stopped, Slow, Normal, Fast);
 
-   -- Actions on the Radar
+   --  Actions on the Radar
    procedure Rotate_Antenna (Speed : Speed_Setting_T);
    procedure Walk_And_Scan;
    procedure Next_Object;
    procedure Get_Closer (Speed : Speed_Setting_T);
 
-   -- Radar status
+   --  Radar status
    function Get_Active_Object_Status return Object_Status_T;
    function Get_Active_Object_Distance return Object_Distance_Km_T;
    function Get_Running_Speed return Speed_Kph_T;
 
-   -- Radar feedback
+   --  Radar feedback
    procedure Update_E_T_A (E_T_A : Float);
    procedure Update_No_E_T_A;
 
-   -- Print status from internal radar state
+   --  Print status from internal radar state
    procedure Time_Step;
 
-   -- Print status, ignores internal radar state
+   --  Print status, ignores internal radar state
    procedure Time_Step
      (Radar_Angle        : Float; Time_To_Arrival : Float;
       John_Connor_Status : String);
