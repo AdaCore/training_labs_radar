4d5d983e2cb975a82f6b107692c8d7c95ff5fb42
Leo Germond <germond@adacore.com>
2024-08-20 15:30:49 +0200

    update doc theme

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+from pathlib import Path
+import time
+import toml
+
+ROOT = Path(__file__).parents[1]
+with open(ROOT / 'alire.toml') as f:
+    alr_config = toml.load(f)
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 project = 'Labs Radar'
-copyright = '2024, Léo Germond'
-author = 'Léo Germond'
+
+def get_copyright():
+    y_start = '2023'
+    y = time.strftime('%Y')
+    return  (f'{y_start}-{y}' if y != y_start else y_start) + ', AdaCore'
+
+copyright = get_copyright()
+author = ",".join(alr_config['authors'])
+version = alr_config['version']
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,5 +42,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
